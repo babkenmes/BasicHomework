506c96891a3e61caef0c1fe34136326778c6c09a
Babken Mesropyan <babken@topsoft.am>
2021-11-30 12:03:41 +0400

    Added people basic apis

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const mongoose = require("mongoose")
+const bodyParser = require('body-parser')
 const Person = require("./Models/Person")
-
 
 const { DB_CONNECTION_STRING } = process.env
 
@@ -13,8 +13,7 @@ const db = mongoose.connection
 
 db.once('open', async function () {
 
-    const john = new Person({ name: "John" })
-    await john.save()
+
     console.log('MongoDB successfully connected');
 
 })
@@ -23,22 +22,48 @@ db.on('error', function (err) {
     console.log(err);
 });
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 
-app.get('/:id', async (req, res) => {
+app.get('/api/people', async (req, res) => {
+    try {
+        const people = await Person.find()
+        res.send(people)
+    }
+    catch (err) {
+        res.status(500).send(err)
+    }
+})
 
-    const id = req.params.id
-    const person = await Person.findById(id)
-    res.send(person)
-
+app.get('/api/people/:id', async (req, res) => {
+    try {
+        const id = req.params.id
+        const person = await Person.findById(id)
+        res.send(person)
+    }
+    catch (err) {
+        res.status(500).send(err)
+    }
 })
 
 
-app.get('/getyoungsters', async (req, res) => {
 
-    const people = await Person.find()
-    res.send(people)
+app.post('/api/people', async (req, res) => {
 
+    try {
+        const { name, salary, age } = req.body;
+        const person = new Person({ name, salary, age })
+        await person.save()
+        res.send(person)
+    }
+    catch(err){
+        res.status(500).send(err)
+    }
+    
 })
 
 
